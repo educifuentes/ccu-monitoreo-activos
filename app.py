98395ac73c5900318a96aabe8bb512f7a789cdcb
educifuentes <edu.cifuentes@gmail.com>
2026-01-30 17:49:13 -0300

    feat: Add a data profiling page for staging census data and update the censos page to display intermediate data.

--- app.py
+++ app.py
@@ -15,12 +15,13 @@ documentation_page = st.Page("pages/2_tools/documentation.py", title="Documentac
 censos_page = st.Page("pages/3_data_build/1_censos.py", title="Censos", icon=":material/dashboard:")
 locales_page = st.Page("pages/3_data_build/2_locales.py", title="Locales", icon=":material/inventory_2:")
 reportes_ccu_page = st.Page("pages/3_data_build/3_reportes_ccu.py", title="Reportes CCU", icon=":material/dashboard:")
+data_profilling_page = st.Page("pages/3_data_build/data_profilling.py", title="Data Profiling", icon=":material/dashboard:")
 
 # current page
 pg = st.navigation({
     "Vistas": [general_page, detalles_page],
     "Herramientas": [explore_page, validations_page, documentation_page],
-    "Dev - Modelos": [censos_page, locales_page, reportes_ccu_page],
+    "Dev - Modelos": [censos_page, locales_page, reportes_ccu_page, data_profilling_page],
 })
 
 with st.sidebar:
--- pages/3_data_build/data_profilling.py
+++ pages/3_data_build/data_profilling.py
@@ -1,19 +1,15 @@
 import streamlit as st
-import pandas as pd
 from pygwalker.api.streamlit import StreamlitRenderer
 from models.staging._stg_censos_censo_2 import stg_censos_censo_2
-from models.intermediate._int_censos_censo_2 import int_censos_censo_2
 
 
-st.header("Censos")
+st.header("Data Profiling")
 
 st.subheader("Censo 2 - 2025")
+
 # Load the data
 stg_censos_2 = stg_censos_censo_2()
-int_censos_2 = int_censos_censo_2()
-
-st.dataframe(int_censos_2)
 
 # Use the StreamlitRenderer to embed the explorer
-# renderer = StreamlitRenderer(stg_censos_2)
-# renderer.explorer()
+renderer = StreamlitRenderer(stg_censos_2)
+renderer.explorer()
