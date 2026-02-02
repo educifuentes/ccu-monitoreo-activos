4ac44ff2558acf00501b322f347bfc04ee183abd
educifuentes <edu.cifuentes@gmail.com>
2026-02-02 09:48:09 -0300

    feat - stg base norm locales

---
+++ models/staging/_stg_base_norm_locales.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+def stg_base_norm_locales():
+    # Define file path
+    file_path = "seeds/base_normalizada/base_normalizada - locales.csv"
+    
+    # Load CSV
+    df = pd.read_csv(file_path)
+
+    # rename
+    rename_dict = {
+        "id": "local_id",
+        "RAZON SOCIAL": "razon_social",
+        "RUT": "rut",
+        "DIRECCIÓN": "direccion",
+        "REGIÓN": "region",
+        "CIUDAD":  "ciudad", 
+        "Nombre de Fantasía ": "nombre_fantasia",
+        "Nombre de Fantasía 2": "nombre_fantasia_2"
+    }
+
+    df.rename(columns=rename_dict, inplace=True)
+
+    selected_columns = [
+        "local_id",
+        "razon_social",
+        "rut",
+        "direccion",
+        "region",
+        "ciudad",
+        "nombre_fantasia" ]
+
+    # 3. Output Information
+    # print("--- DataFrame Head ---")
+    # print(df.iloc[:, :5].head())
+
+    print("\n--- List of Column Names ---")
+    for i, col in enumerate(df.columns):
+        print(f"{i}: {col}")
+
+    df = df[selected_columns]
+        
+    return df
--- pages/3_data_build/2_locales.py
+++ pages/3_data_build/2_locales.py
@@ -1,9 +1,11 @@
 import streamlit as st
-from models.marts.dim_locales import marts_dim_locales
+# from models.marts.dim_locales import marts_dim_locales
+from models.staging._stg_base_norm_locales import stg_base_norm_locales
+
+# load
+locales_df = stg_base_norm_locales()
 
 
 st.header("Locales")
-
-locales = marts_dim_locales()
-
-st.dataframe(locales)   
+st.write(locales_df.shape)
+st.dataframe(locales_df)   
