5fcc318a277a80a4160df76e4e773dc175fdfd0a
educifuentes <edu.cifuentes@gmail.com>
2026-02-02 17:54:22 -0300

    refactor: Reimplement `dim_locales` using `_stg_base_norm_locales` with added string cleaning and display it in the finals page.

--- models/marts/dim_locales.py
+++ models/marts/dim_locales.py
@@ -1,39 +1,17 @@
-from models.staging._stg_censos_censo_2 import stg_censos_censo_2
+# from models.staging._stg_censos_censo_2 import stg_censos_censo_2
 
-def load_locales_from_censo_2():
-    stg_censos_2_df = stg_censos_censo_2()
-
-    rename_dict = {
-        "ID PK": "local_id",
-        "ID CCU": "ccu_id",
-        "Dirección": "direccion",
-        "Comuna": "comuna",
-        "Región": "region",
-        "Latitud": "latitud",
-        "Longitud": "longitud",
-        "NOMBRE LOCAL FANTASIA": "nombre_fantasia",
-    }
-
-    stg_censos_2_df.rename(columns=rename_dict, inplace=True)
-
-    selected_columns = [
-        "local_id",
-        "ccu_id",
-        "nombre_fantasia",
-        # geolocation
-        "direccion",
-        "comuna",
-        "region",
-        "latitud",
-        "longitud",
-    ]
-
-    return stg_censos_2_df[selected_columns]
+from models.staging._stg_base_norm_locales import stg_base_norm_locales
 
 
-def marts_dim_locales():
-    
-    return load_locales_from_censo_2()
+def dim_locales():
+    locales_df = stg_base_norm_locales()
 
-if __name__ == "__main__":
-    print(marts_dim_locales().head())
+    # clean and titleize
+    locales_df["razon_social"] = locales_df["razon_social"].str.strip().str.title()
+    locales_df["rut"] = locales_df["rut"].str.strip()
+    locales_df["direccion"] = locales_df["direccion"].str.strip().str.title()
+    locales_df["region"] = locales_df["region"].str.strip().str.title()
+    locales_df["ciudad"] = locales_df["ciudad"].str.strip().str.title()
+    locales_df["nombre_fantasia"] = locales_df["nombre_fantasia"].str.strip().str.title()
+
+    return locales_df
--- pages/3_data_build/3_finals.py
+++ pages/3_data_build/3_finals.py
@@ -1,11 +1,19 @@
 import streamlit as st
 
 from models.marts.fct_censos import fct_censos
+from models.marts.dim_locales import dim_locales
 
 fct_censos_df = fct_censos()
+dim_locales_df = dim_locales()
 
 st.header("Finales")
 
 st.markdown("### FCT Censos")
 st.write(fct_censos_df.shape)
 st.dataframe(fct_censos_df)
+
+
+st.markdown("### DIM Locales")
+st.write(dim_locales_df.shape)
+st.dataframe(dim_locales_df)
+
