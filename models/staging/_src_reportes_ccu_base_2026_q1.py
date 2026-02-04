ef86892b17b42e4e6c39ac5343953263a3fe2edb
educifuentes <edu.cifuentes@gmail.com>
2026-02-04 15:58:21 -0300

    feat: YAML loader utility

--- models/staging/_src_reportes_ccu_base_2026_q1.py
+++ models/staging/_src_reportes_ccu_base_2026_q1.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from utilities.yaml_loader import get_table_config
+
 def stg_reportes_ccu_base_2026_q1():
-    # Define file path
-    file_path = "seeds/reportes_ccu/Base Solicitada.xlsx - Hoja1.csv"
+    # Fetch configuration from YAML
+    config = get_table_config(source_name="reportes_ccu", table_name="base_solicitada")
+    file_path = config.get('filename')
     
     # Load CSV
     df = pd.read_csv(file_path)
