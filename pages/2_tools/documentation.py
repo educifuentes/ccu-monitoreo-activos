aaedd353fd5ad08842e81222284bdd3719730aa9
educifuentes <edu.cifuentes@gmail.com>
2026-02-23 16:03:46 -0300

    feat - update doc page

--- pages/2_tools/documentation.py
+++ pages/2_tools/documentation.py
@@ -18,22 +18,22 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs([
 ])
 
 with tab1:
-    dim_locales_path = os.path.abspath("models/marts/_dim_locales.yml")
+    dim_locales_path = os.path.abspath("models/raw/marts/_dim_locales.yml")
     render_model_docs(dim_locales_path)
 
 with tab2:
-    fct_censos_path = os.path.abspath("models/marts/_fct_censos.yml")
+    fct_censos_path = os.path.abspath("models/raw/marts/_fct_censos.yml")
     render_model_docs(fct_censos_path)
 
 with tab3:
-    fct_comodatos_path = os.path.abspath("models/marts/_fct_comodatos.yml")
-    render_model_docs(fct_comodatos_path)
+    fct_bases_ccu_path = os.path.abspath("models/raw/marts/_fct_bases_ccu.yml")
+    render_model_docs(fct_bases_ccu_path)
 
 with tab4:
-    fct_contratos_path = os.path.abspath("models/marts/_fct_contratos.yml")
+    fct_contratos_path = os.path.abspath("models/raw/marts/_fct_contratos.yml")
     render_model_docs(fct_contratos_path)
 
 with tab5:
     st.header("Métricas")
-    fct_metricas_path = os.path.abspath("models/marts/metrics/_metrics_docs.yml")
+    fct_metricas_path = os.path.abspath("models/raw/marts/metrics/_metrics_docs.yml")
     render_model_docs(fct_metricas_path, kind="metrics")
