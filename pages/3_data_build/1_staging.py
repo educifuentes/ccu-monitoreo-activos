52946f2fa7c89017ca89d030d7cf60759eaa4d9e
educifuentes <edu.cifuentes@gmail.com>
2026-03-02 13:36:01 -0300

    style - columns per line

--- pages/3_data_build/1_staging.py
+++ pages/3_data_build/1_staging.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 from models.raw.staging.censos._stg_censos_censo_2 import stg_censos_censo_2
 from models.raw.staging.censos._stg_censos_censo_1 import stg_censos_censo_1
+from models.raw.staging.censos._stg_censos_censo_2026_1 import stg_censos_censo_2026_1
+
 from models.raw.staging.base_normalizada._stg_base_norm_censo_1 import stg_base_norm_censo_1
 from models.raw.staging.base_normalizada._stg_base_norm_locales import stg_base_norm_locales
 
@@ -24,36 +26,7 @@ tab1, tab2, tab3, tab4 = st.tabs([
     ":material/checklist_rtl: Censos",
     ":material/assignment: Bases CCU",
     ":material/book: Base Norm Original"
-], default=":material/book: Base Norm Original")
-
-with tab3:
-    st.header("Base CCU 2026 Q1")
-    st.markdown("Carga directa desde el reporte de CCU.")
-    df = stg_reportes_ccu_base_2026_q1()
-    filtered_df = explorer_de_datos(df)
-    render_model_ui(filtered_df)
-
-    st.divider()
-    
-
-    
-
-
-with tab2:
-    st.header("Censos")
-    
-    st.subheader("Censo 2 (2025)")
-    df2 = stg_censos_censo_2()
-    filtered_df2 = explorer_de_datos(df2)
-    render_model_ui(filtered_df2)
-    
-    st.divider()
-    
-    st.subheader("Censo 1 (2024)")
-    st.warning("Data histórica - Usar con precaución.")
-    df1 = stg_censos_censo_1()
-    filtered_df1 = explorer_de_datos(df1)
-    render_model_ui(filtered_df1)
+], default=":material/checklist_rtl: Censos")
 
 with tab1:
     st.header("Bases Normalizadas")
@@ -69,6 +42,37 @@ with tab1:
     df_c1 = stg_base_norm_censo_1()
     filtered_df_c1 = explorer_de_datos(df_c1)
     render_model_ui(filtered_df_c1)
+
+with tab2:
+    st.header("Censos")
+
+    st.subheader("Censo 2026_1")
+    df2026_1 = stg_censos_censo_2026_1()
+    filtered_df2026_1 = explorer_de_datos(df2026_1)
+    render_model_ui(filtered_df2026_1)
+        
+    st.subheader("Censo 2 (2025)")
+    df2 = stg_censos_censo_2()
+    filtered_df2 = explorer_de_datos(df2)
+    render_model_ui(filtered_df2)
+    
+    
+    # st.subheader("Censo 1 (2024)")
+    # st.warning("Data histórica - Usar con precaución.")
+    # df1 = stg_censos_censo_1()
+    # filtered_df1 = explorer_de_datos(df1)
+    # render_model_ui(filtered_df1)
+
+
+with tab3:
+    st.header("Base CCU 2026 Q1")
+    st.markdown("Carga directa desde el reporte de CCU.")
+    df = stg_reportes_ccu_base_2026_q1()
+    filtered_df = explorer_de_datos(df)
+    render_model_ui(filtered_df)
+
+    st.divider()
+
 
 with tab4:
     st.header("Data Original")
--- utilities/ui_components.py
+++ utilities/ui_components.py
@@ -29,7 +29,7 @@ def render_model_ui(df, source_name=None, table_name=None):
 
     # st.markdown(f"Source: `{source_name}.{table_name}`")
     st.write(df.shape)
-    st.code(df.columns.tolist())
+    st.code("\n".join(df.columns))
     # Format dtypes as a single line: col1: type1 | col2: type2
     dtypes_str = " | ".join([f"{col}: {dtype}" for col, dtype in df.dtypes.items()])
     st.code(dtypes_str)
