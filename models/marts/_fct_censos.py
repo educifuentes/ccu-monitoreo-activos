03589dc260005c66685cce6057cbbb1a3bbe1b31
educifuentes <edu.cifuentes@gmail.com>
2026-03-23 18:48:04 -0300

    feat - refine fct censos

--- models/marts/_fct_censos.py
+++ models/marts/_fct_censos.py
@@ -6,14 +6,7 @@ from models.intermediate.censos._int_censos__censo_2026_1 import int_censos__cen
 
 from models.intermediate.censos._int_censos__censo_2024_2 import int_censos__censo_2024_2
 
-
-def fct_censos_2026():
-    # blends corpa and pk agencias
-
-    df_censo_2026_1 = int_censos__censo_2026_1_agencia_pk()
-    df_censo_2026_1_agencia_corpa = int_censos__censo_2026_1_agencia_corpa()
-
-    selected_columns = [
+selected_columns = [
         "local_id",
         # locales info
         "razon_social",
@@ -53,6 +46,13 @@ def fct_censos_2026():
 
     ]
 
+def fct_censos_2026():
+    # blends corpa and pk agencias
+    # tenporal para reporte censo 2026 on demand
+
+    df_censo_2026_1 = int_censos__censo_2026_1_agencia_pk()
+    df_censo_2026_1_agencia_corpa = int_censos__censo_2026_1_agencia_corpa()
+
     # 5. Union and Final Processing
     df = safe_concat_with_columns([df_censo_2026_1, df_censo_2026_1_agencia_corpa], selected_columns)
 
@@ -63,44 +63,18 @@ def fct_censos():
     # 1. Load intermediate models
     df_censo_2024_2 = int_censos__censo_2024_2()
     df_censo_2025_2 = int_censos__censo_2025_2()
-    df_censo_2026_1 = fct_censos_2026()
-
-    selected_columns = [
-        "local_id",
-        # metadata censo
-        "periodo",
-        "fecha",
-        "agencia",
-        "permite_censo",
-        "motivo_no_censo",
-        # activos
-        "schoperas_total",
-        "schoperas_ccu",
-        "salidas",
-        # accion
-        "accion_ccu",
-        "instalo",
-        "disponibilizo",
-        # marcas
-        "marcas",
-        "marcas_abinbev",
-        "marcas_kross",
-        "marcas_ccu",
-        "marcas_otras",
-        # marcas - listados
-        "marcas_otras_listado"
-    
-    ]
-
-    # df_censo_2024_2 = df_censo_2024_2[selected_columns]
-    # df_censo_2025_2 = df_censo_2025_2[selected_columns]
-    # df_censo_2026_1 = df_censo_2026_1[selected_columns]
-
-    # df = pd.concat([df_censo_2024_2, df_censo_2025_2, df_censo_2026_1], ignore_index=True)
-
+    df_censo_2026_1_agencia_pk = int_censos__censo_2026_1_agencia_pk()
+    df_censo_2026_1_agencia_corpa = int_censos__censo_2026_1_agencia_corpa()
 
     # 5. Union and Final Processing
-    df = safe_concat_with_columns([df_censo_2024_2, df_censo_2025_2, df_censo_2026_1], selected_columns)
+    df = safe_concat_with_columns(
+        [df_censo_2024_2, 
+        df_censo_2025_2, 
+        df_censo_2026_1_agencia_pk, 
+        df_censo_2026_1_agencia_corpa
+        ], 
+        selected_columns
+    )
 
     return df
 
