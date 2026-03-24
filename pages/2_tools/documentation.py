b7b813d130d35adc78d8056639654f2befe2e099
educifuentes <edu.cifuentes@gmail.com>
2026-03-24 10:34:34 -0300

    fixup

--- pages/2_tools/documentation.py
+++ pages/2_tools/documentation.py
@@ -7,45 +7,50 @@ st.set_page_config(page_title="Documentación", layout="wide")
 
 st.title(f"{ICONS['documentation']} Documentación")
 
-
-
-st.subheader("Marcas y Grupos Corporativos")
-with open("documentation/_marcas_table.md", "r", encoding="utf-8") as f:
-    st.markdown(f.read())
-
-
-st.divider()
-
 st.subheader("Tablas")
 
 # Create tabs for organization
-tab1, tab2, tab3, tab4, tab5 = st.tabs([
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
     f"{ICONS['clientes']} Clientes",
     f"{ICONS['censos']} Censos",
     f"{ICONS['bases_ccu']} Bases CCU",
     f"{ICONS['contratos']} Contratos",
-    f"{ICONS['metrics']} Metricas"
+    f"{ICONS['metrics']} Metricas",
+    f"{ICONS['metrics']} Marcas",
+
 ])
 
 
 
 with tab1:
+    st.header("Clientes")
+
     # dim_clientes_path = os.path.abspath("models/raw/marts/_dim_clientes.yml")
     # render_model_docs(dim_clientes_path)
 
 with tab2:
+    st.header("Censos")
     # fct_censos_path = os.path.abspath("models/raw/marts/_fct_censos.yml")
     # render_model_docs(fct_censos_path)
 
 with tab3:
+    st.header("Bases CCU")
     fct_bases_ccu_path = os.path.abspath("models/marts/fct_bases_ccu.yml")
     render_model_docs(fct_bases_ccu_path)
 
 with tab4:
+    st.header("Contratos")
     # fct_contratos_path = os.path.abspath("models/raw/marts/_fct_contratos.yml")
     # render_model_docs(fct_contratos_path)
 
 with tab5:
-    # st.header("Métricas")
+    st.header("Métricas")
     # fct_metricas_path = os.path.abspath("models/raw/marts/metrics/_metrics_docs.yml")
     # render_model_docs(fct_metricas_path, kind="metrics")
+
+with tab6:
+    st.header("Marcas y Grupos Corporativos")
+    with open("documentation/_marcas_table.md", "r", encoding="utf-8") as f:
+        st.markdown(f.read())
+
+
