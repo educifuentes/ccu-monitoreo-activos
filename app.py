0650380c55f861203f0f315f2ff5c09b94525521
educifuentes <edu.cifuentes@gmail.com>
2026-03-26 13:44:23 -0300

    fix local diplay pages

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ nav_dict = {
 
 
 # Only expose the development environment tabs locally
-is_local = os.environ.get("ENVIRONMENT", "cliente").lower() == "cliente"
+is_local = os.environ.get("ENVIRONMENT", "local").lower() == "local"
 if is_local:
     nav_dict["Desarrollo"] = [
         st.Page("pages/3_dev/1_staging.py", title="Staging", icon=":material/dashboard:"),
